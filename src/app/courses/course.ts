4deb8e4df5aac5d2c8508acd95c3caea00ebde1a
Caiofpimentel <caio-pimentel@hotmail.com>
2022-07-20 20:53:58 -0300

    Commit inicial

---
+++ src/app/courses/course.ts
@@ -0,0 +1,14 @@
+
+
+export class Course {
+    id!: number;
+    name!: string;
+    imgUrl!: string;
+    price!: number;
+    code!: string;
+    duration!: number;
+    rating!: number;
+    description!: string;
+    releaseDate!: string; 
+
+}
